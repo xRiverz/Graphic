c3a3b43454dfc0229ea8fd752f472de108c93fda
Anhar AG <85056292+xRiverz@users.noreply.github.com>
2021-11-29 16:26:41 +0300

    Update Contents.swift

--- GraphicsPrimitive.playground/Contents.swift
+++ GraphicsPrimitive.playground/Contents.swift
@@ -7,29 +7,13 @@ struct Point{
 }
 
 struct Line{
-    var start : Point
+    var begin : Point
     var end : Point
     
     func length() -> Double{
-        return sqrt( pow(end.x - start.x, 2) + pow(end.y - start.y, 2) )
+        return sqrt( pow(end.x - begin.x, 2) + pow(end.y - begin.y, 2) )
     }
 }
 
-struct Triangle{
-    var points : [Point]
-    
-    func area() -> Double {
-        var a = points[0]
-        var b = points[1]
-        var c = points[2]
-        
-        return (a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) * 0.5
-    }
-}
-
-var line = Line(start: Point(x: 1, y: 2), end: Point(x: 4, y: 6))
+var line = Line(begin: Point(x: 1, y: 2), end: Point(x: 9, y: 6))
 print(line.length())
-
-var triangle = Triangle(points: [Point(x: 0, y: 0), Point(x: 3, y: 0), Point(x: 3, y: 9)])
-
-print(triangle.area())
